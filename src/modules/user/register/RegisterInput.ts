b393d6476b41f8d193b668d24bf66a445a3f5ac3
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-06-07 01:02:02 +0900

    9

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,14 +1,18 @@
+import { Length, IsEmail } from "class-validator";
 import { InputType, Field } from "type-graphql";
 
 @InputType()
 export class RegisterInput {
   @Field()
+  @Length(1, 255)
   firstName: string;
 
   @Field()
+  @Length(1, 255)
   lastName: string;
 
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
