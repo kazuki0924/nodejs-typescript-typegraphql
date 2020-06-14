daffa3d59eaaf8e748214b35b9059478f2eaa9a0
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-06-14 23:29:19 +0900

    28

--- src/modules/user/register/Register.test.ts
+++ src/modules/user/register/Register.test.ts
@@ -1,19 +1,43 @@
-import { graphql } from 'graphql'
 import { Connection } from 'typeorm'
 
 import { testConn } from '../../../test-utils/testConn'
+import { gCall } from '../../../test-utils/gCall'
 
 let conn: Connection;
 beforeAll(async () => {
-  await testConn();
+  conn = await testConn();
 });
 
 afterAll(async () => {
   await conn.close();
 });
 
+const registerMutation = `
+mutation Register($data: RegisterInput!) {
+  register(data: $data) {
+    id
+    firstName
+    lastName
+    email
+    name
+  }
+}
+`;
+
 describe("Register", () => {
-  it("create user", () => {
-    graphql({});
+  it("create user", async () => {
+    console.log(
+      await gCall({
+        source: registerMutation,
+        variableValues: {
+          data: {
+            firstName: "John",
+            lastName: "Smith",
+            email: "johnsmith@example.com",
+            password: "password",
+          },
+        },
+      })
+    );
   });
 });
