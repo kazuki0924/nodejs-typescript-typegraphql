f53999cc3539df206faafe831dbb92dfdf996d6b
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-06-27 14:49:34 +0900

    36

--- src/modules/user/CreateUser.ts
+++ src/modules/user/CreateUser.ts
@@ -52,20 +52,24 @@ import {
   ClassType,
   Field,
   InputType,
+  UseMiddleware,
 } from "type-graphql";
 import { User } from "../../entity/User";
 import { RegisterInput } from "./register/RegisterInput";
 import { Product } from "../../entity/Product";
+import { Middleware } from "type-graphql/dist/interfaces/Middleware";
 
 function createResolver<T extends ClassType, X extends ClassType>(
   suffix: string,
   returnType: T,
   inputType: X,
-  entity: any
+  entity: any,
+  middleware?: Middleware<any>[]
 ) {
   @Resolver({ isAbstract: true })
   class BaseResolver {
     @Mutation(() => returnType, { name: `create${suffix}` })
+    @UseMiddleware(...(middleware || []))
     async createUser(@Arg("data", () => inputType) data: any) {
       return entity.create(data).save();
     }
